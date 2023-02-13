c6249289c31f06350fd7a37094c5601928cad12f
Nayyer Ahmad <nayyer.amd@gmail.com>
2023-02-14 01:36:56 +0200

    Added prompt to create options

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
 
 function App() {
-  const [options, setOptions] = useState([
-    { id: 1, name: "Option 1", votes: 0 },
-    { id: 2, name: "Option 2", votes: 0 },
-    { id: 3, name: "Option 3", votes: 0 },
-  ]);
+  const [options, setOptions] = useState([]);
+
+  const handleCreateOptions = () => {
+    const numberOfOptions = parseInt(
+      window.prompt("Enter the number of options:")
+    );
+
+    if (numberOfOptions) {
+      const newOptions = [];
+      for (let i = 0; i < numberOfOptions; i++) {
+        const name = window.prompt(`Enter name for option ${i + 1}:`);
+        newOptions.push({ id: i + 1, name, votes: 0 });
+      }
+      setOptions(newOptions);
+    }
+  };
 
   const handleVote = (id) => {
     setOptions(
@@ -19,45 +30,48 @@ function App() {
 
   const handleRefresh = () => {
     if (window.confirm("Are you sure you want to refresh the votes?")) {
-      setOptions([
-        { id: 1, name: "Option 1", votes: 0 },
-        { id: 2, name: "Option 2", votes: 0 },
-        { id: 3, name: "Option 3", votes: 0 },
-      ]);
+      setOptions(options.map((option) => ({ ...option, votes: 0 })));
     }
   };
 
   return (
     <div className="container">
       <h1>Voting App</h1>
-      <div className="options">
-        {options.map((option) => (
-          <div className="option" key={option.id}>
-            <h3>{option.name}</h3>
-            <p>Votes: {option.votes}</p>
-            <button onClick={() => handleVote(option.id)}>Vote</button>
-          </div>
-        ))}
-      </div>
-      <div className="votes">
-        <table>
-          <thead>
-            <tr>
-              <th>Option</th>
-              <th>Votes</th>
-            </tr>
-          </thead>
-          <tbody>
+      {options.length === 0 && (
+        <button onClick={handleCreateOptions}>Create Options</button>
+      )}
+      {options.length > 0 && (
+        <div>
+          <div className="options">
             {options.map((option) => (
-              <tr key={option.id}>
-                <td>{option.name}</td>
-                <td>{option.votes}</td>
-              </tr>
+              <div className="option" key={option.id}>
+                <h3>{option.name}</h3>
+                <p>Votes: {option.votes}</p>
+                <button onClick={() => handleVote(option.id)}>Vote</button>
+              </div>
             ))}
-          </tbody>
-        </table>
-        <button onClick={handleRefresh}>Refresh</button>
-      </div>
+          </div>
+          <div className="votes">
+            <table>
+              <thead>
+                <tr>
+                  <th>Option</th>
+                  <th>Votes</th>
+                </tr>
+              </thead>
+              <tbody>
+                {options.map((option) => (
+                  <tr key={option.id}>
+                    <td>{option.name}</td>
+                    <td>{option.votes}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <button onClick={handleRefresh}>Refresh</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }

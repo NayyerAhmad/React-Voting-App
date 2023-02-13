6a8b68a2eb565136226c4a51a65a451fde55f6c5
Nayyer Ahmad <nayyer.amd@gmail.com>
2023-02-14 01:32:46 +0200

    Added refresh button with confirmation prompt

--- src/App.js
+++ src/App.js
@@ -2,25 +2,62 @@ import React, { useState } from "react";
 
 function App() {
   const [options, setOptions] = useState([
-    { id: 1, text: "Option 1" },
-    { id: 2, text: "Option 2" },
-    { id: 3, text: "Option 3" }
+    { id: 1, name: "Option 1", votes: 0 },
+    { id: 2, name: "Option 2", votes: 0 },
+    { id: 3, name: "Option 3", votes: 0 },
   ]);
-  const [votes, setVotes] = useState({});
 
-  const handleVote = optionId => {
-    setVotes({ ...votes, [optionId]: (votes[optionId] || 0) + 1 });
+  const handleVote = (id) => {
+    setOptions(
+      options.map((option) =>
+        option.id === id
+          ? { ...option, votes: option.votes + 1 }
+          : option
+      )
+    );
+  };
+
+  const handleRefresh = () => {
+    if (window.confirm("Are you sure you want to refresh the votes?")) {
+      setOptions([
+        { id: 1, name: "Option 1", votes: 0 },
+        { id: 2, name: "Option 2", votes: 0 },
+        { id: 3, name: "Option 3", votes: 0 },
+      ]);
+    }
   };
 
   return (
-    <div>
-      {options.map(option => (
-        <div key={option.id}>
-          <p>{option.text}</p>
-          <p>Votes: {votes[option.id] || 0}</p>
-          <button onClick={() => handleVote(option.id)}>Vote</button>
-        </div>
-      ))}
+    <div className="container">
+      <h1>Voting App</h1>
+      <div className="options">
+        {options.map((option) => (
+          <div className="option" key={option.id}>
+            <h3>{option.name}</h3>
+            <p>Votes: {option.votes}</p>
+            <button onClick={() => handleVote(option.id)}>Vote</button>
+          </div>
+        ))}
+      </div>
+      <div className="votes">
+        <table>
+          <thead>
+            <tr>
+              <th>Option</th>
+              <th>Votes</th>
+            </tr>
+          </thead>
+          <tbody>
+            {options.map((option) => (
+              <tr key={option.id}>
+                <td>{option.name}</td>
+                <td>{option.votes}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <button onClick={handleRefresh}>Refresh</button>
+      </div>
     </div>
   );
 }

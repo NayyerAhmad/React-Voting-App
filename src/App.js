dc2582a31e7a8188c30796860866787242c83141
Nayyer Ahmad <nayyer.amd@gmail.com>
2023-02-14 00:34:30 +0200

    Added UI components to React app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [options, setOptions] = useState([
+    { id: 1, text: "Option 1" },
+    { id: 2, text: "Option 2" },
+    { id: 3, text: "Option 3" }
+  ]);
+  const [votes, setVotes] = useState({});
+
+  const handleVote = optionId => {
+    setVotes({ ...votes, [optionId]: (votes[optionId] || 0) + 1 });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {options.map(option => (
+        <div key={option.id}>
+          <p>{option.text}</p>
+          <p>Votes: {votes[option.id] || 0}</p>
+          <button onClick={() => handleVote(option.id)}>Vote</button>
+        </div>
+      ))}
     </div>
   );
 }
